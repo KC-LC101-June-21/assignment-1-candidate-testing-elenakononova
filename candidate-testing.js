d7447275b0d9db3bb24575fd55c49d4dd900ec73
elenakononova <68666592+elenakononova@users.noreply.github.com>
2021-06-09 19:28:40 +0000

    part 2 done

--- candidate-testing.js
+++ candidate-testing.js
@@ -60,7 +60,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Candidate Name:", ' '+ candidateName);
+  console.log(`Candidate Name: ${candidateName}`);
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
